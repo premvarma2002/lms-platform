a69e88ebbefe80f9566349b3f95943a8da64717a
premvarma2002 <vakilverma1982@gmail.com>
2024-01-31 23:51:23 +0530

    add publish alert banner

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,6 +9,8 @@ import { ChapterTitleForm } from "./[_components]/chapter-title-form";
 import { ChapterDescriptionForm } from "./[_components]/chapter-description-form";
 import { ChapterAccessForm } from "./[_components]/chapter-access-form";
 import { ChapterVideoForm } from "./[_components]/chapter-video-form";
+import { Banner } from "@/components/banner";
+import { ChapterActions } from "./[_components]/chapter-actions";
 
 const ChapterIdPage = async ({
   params,
@@ -42,7 +44,16 @@ const ChapterIdPage = async ({
 
   const completionText = `(${completedFields}/${totalFields})`;
 
+  const isComplete = requiredFields.every(Boolean);
+
   return (
+    <>
+    {!chapter.isPublished && (
+      <Banner 
+        variant="warning"
+        label="This chapter is unpublished. It will not be visible in the course"
+      />
+    )}
     <div className="p-6">
       <div className="flex items-center justify-between">
         <div className="w-full">
@@ -61,6 +72,12 @@ const ChapterIdPage = async ({
                 Complete all fields {completionText}
               </span>
             </div>
+            <ChapterActions
+              disabled={!isComplete}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+              isPublished={chapter.isPublished}
+            />
           </div>
         </div>
       </div>
@@ -111,6 +128,7 @@ const ChapterIdPage = async ({
           </div>
         </div>
       </div>
+      </>
   );
 };
 
