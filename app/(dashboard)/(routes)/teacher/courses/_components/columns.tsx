b2617a11707ee96abf06ac048c63b9b132180a34
premvarma2002 <vakilverma1982@gmail.com>
2024-02-09 22:07:13 +0530

    add course card to search

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -2,9 +2,19 @@
 
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link"
+import { Badge } from "@/components/ui/badge"
+import { cn } from "@/lib/utils"
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -32,6 +42,15 @@ export const columns: ColumnDef<Course>[] = [
             </Button>
         )
     },
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price") || "0");
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "INR"
+      }).format(price);
+
+      return <div>{formatted}</div>
+    }
   },
   {
     accessorKey: "isPublished",
@@ -45,5 +64,42 @@ export const columns: ColumnDef<Course>[] = [
             </Button>
         )
     },
+  cell: ({ row }) => {
+    const isPublished = row.getValue("isPublished") || false;
+
+    return (
+      <Badge className={cn(
+        "bg-slate-500",
+        isPublished && "bg-sky-700"
+      )}>
+        {isPublished ? "Published" : "Draft"}
+      </Badge>
+    )
+    }
   },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+           <Button variant="ghost" className="h-4 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal size={20}/>
+           </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+            <DropdownMenuItem>
+              <Pencil className="h-4 w-4 mr-2"/>
+              Edit
+            </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
 ]
